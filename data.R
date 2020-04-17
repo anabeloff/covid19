9e604fa6ddcd904ec799fe0388a112f737cfac95
anabeloff <anabeloff@gmail.com>
2020-04-17 16:47:45 +0000

    data update

--- data.R
+++ data.R
@@ -1,9 +1,10 @@
 ## DATA ##
-setwd("data")
 # load functions
 source("covid_functions.R")
 
 # UPDATE DATA 
+setwd("data")
+
 #system("wget -q https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv -O data/time_series_covid19_deaths_global.csv")
 #system("wget -q https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv -O data/time_series_covid19_confirmed_global.csv")
 
@@ -14,27 +15,27 @@ message("Data downloaded.\n")
 
 ## Data from source
 # Confirmed
-time_confirmed_global <- read.csv("data/time_series_covid19_confirmed_global.csv")
+time_confirmed_global <- read.csv("time_series_covid19_confirmed_global.csv")
 
 con_dt <- time_confirmed_global[c(-1,-3,-4)]
 
 dt_C <- final_dt_day(data_tbl = con_dt)
 dt <- final_dt(data_tbl = con_dt)
 
-saveRDS(dt_C, "data/dt_confirmed.rda")
-saveRDS(dt, "data/dt_confirmed_week.rda")
+saveRDS(dt_C, "dt_confirmed.rda")
+saveRDS(dt, "dt_confirmed_week.rda")
 
 message("Confirmed cases saved.\n")
 
 # Deaths
-time_deaths_global <- read.csv("data/time_series_covid19_deaths_global.csv")
+time_deaths_global <- read.csv("time_series_covid19_deaths_global.csv")
 con_dtD <- time_deaths_global[c(-1,-3,-4)]
 
 dtD_D <- final_dt_day(data_tbl = con_dtD)
 dtD <- final_dt(data_tbl = con_dtD)
 
-saveRDS(dtD_D, "data/dt_deaths.rda")
-saveRDS(dtD, "data/dt_deaths_week.rda")
+saveRDS(dtD_D, "dt_deaths.rda")
+saveRDS(dtD, "dt_deaths_week.rda")
 
 message("Death cases saved.\n")
 message("COMPLETE!\n")
