4b272414f390ce4ef2586d70577d18c54852a733
anabeloff <anabeloff@gmail.com>
2020-04-20 13:00:49 +0000

    Country list update

--- app.R
+++ app.R
@@ -17,7 +17,7 @@ library(shinythemes)
 # Selected countries list.
 countries = c("Canada", "US", "Japan", "Korea, South", "Russia", "Spain", "China", "Thailand", "France", 'Taiwan*', "Italy",
               "Singapore", "United Kingdom", "Germany", "Poland", "Ukraine", "Portugal", "India", "Israel", "Turkey", "Hong Kong",
-              "Brazil", "Honduras", "Cuba", "Laos", "Cambodia", "Macau", "Mexico", "Georgia", "Iran")
+              "Brazil", "Honduras", "Cuba", "Laos", "Cambodia", "Macau", "Mexico", "Georgia", "Iran", "Sweden", "Finland", "Australia", "New Zealand")
 countries = sort(countries)
 
 # Check Box list
